5f6d7190958ae508b587639b4b2241289ebbc5d1
Adam Anderson <adam@excessneo.co.uk>
2017-12-07 20:18:44 +0000

    Initial boilerplate commit

---
+++ src/main/java/uk/co/excessneo/TechTest.java
@@ -0,0 +1,13 @@
+package uk.co.excessneo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechTest
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(TechTest.class, args);
+    }
+}
